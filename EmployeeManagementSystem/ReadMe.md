52e47aca47eaf381ffce31b06fa2bc20585b76c0
Adil Enver <Adiljan.anwar@gmail.com>
2023-02-04 10:54:19 +0300

    Employee Management System with Thymeleaf + SpringBoot

---
+++ EmployeeManagementSystem/ReadMe.md
@@ -0,0 +1,52 @@
+# Employee Management System
+
+---
+
+## Project Requirements
+Create a web application for Employee Management System
+User should be able to:
+- Get All the employees
+- Add a new employee
+- Update an employee
+- Delete an employee
+
+---
+## Application Architecture
+![](screenshot/Application Architecture.png)
+
+---
+## Technologies Used:
+1. Spring Boot 2.2.6 +
+2. Spring Framework 5.2.5 +
+3. Maven 3.2 +
+4. Java 8
+5. Spring Data Jpa (Hibernate)
+6. Thymeleaf
+
+---
+
+## Steps:
+1. Create and Setup Spring Boot Project in Intellij IDEA Ultimate
+2. Configure MySQL Database
+3. Implement End-to-End List Employee Feature
+4. Implement End-to-End Add Employee Feature
+5. Implement End-to-End Update Employee Feature
+6. Implement End-to-End Delete Employee Feature
+
+---
+
+```
+Results:
+```
+1. Save Employee :
+![](screenshot/saveEmployee.png)
+
+---
+2. Update Employee:
+![](screenshot/UpdateEmployee.png)
+
+---
+3. Employee List :
+![](screenshot/Employee List.png)
+
+---
---
+++ EmployeeManagementSystem/src/main/java/com/adilenver/controller/EmployeeController.java
@@ -0,0 +1,83 @@
+package com.adilenver.controller;
+
+import com.adilenver.model.Employee;
+import com.adilenver.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    // Display List of employees
+    // Creating method handler for homepage (index.html) to display a list of employees
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("listEmployees",employeeService.getAllEmployees());
+        return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        // Create model attribute to bind data
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        // Save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") Long id,Model model){
+
+        // Get employee form the service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // Set employee as a model attribute to pre-populate the form
+        model.addAttribute("employee",employee);
+        return "update_employee";
+    }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") Long id){
+
+        // Call delete employee method
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+
+        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List<Employee> listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listEmployees", listEmployees);
+        return "index";
+    }
+
+}
